96b90a936effe25fb60d3284bbced3674bd2f798
Shadowfacts <akorde@icloud.com>
2015-03-25 20:58:05 -0400

    Logging stuff

---
+++ src/main/java/net/shadowfacts/shadowlib/log/LogLevel.java
@@ -0,0 +1,12 @@
+package net.shadowfacts.shadowlib.log;
+
+/**
+ * Valid levels for use with {@link net.shadowfacts.shadowlib.log.Logger}
+ * @author shadowfacts
+ */
+public enum LogLevel {
+	DEBUG,
+	INFO,
+	WARN,
+	ERROR
+}
