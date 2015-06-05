e0ef8e25f8065758add0d15af4afbda0a2733bd9
Shadowfacts <me@shadowfacts.net>
2015-06-04 22:06:16 -0400

    Add forEachInDirectory utility method

---
+++ src/main/java/net/shadowfacts/shadowlib/util/FileUtils.java
@@ -0,0 +1,23 @@
+package net.shadowfacts.shadowlib.util;
+
+import java.io.File;
+import java.util.function.Consumer;
+
+/**
+ * Various utilities realted to the file system.
+ * @author shadowfacts
+ */
+public class FileUtils {
+
+	public static void forEachInDirectory(File dir, Consumer<File> consumer) {
+		File[] dirListing = dir.listFiles();
+		if (dirListing != null) {
+			for (File f : dirListing) {
+				consumer.accept(f);
+			}
+		} else {
+			throw new NullPointerException("dirListing was null");
+		}
+	}
+
+}
