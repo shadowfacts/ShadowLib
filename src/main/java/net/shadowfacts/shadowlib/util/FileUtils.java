d4aad0fc9cccefad24e266dc8fdaef1b7d5e6408
Shadowfacts <me@shadowfacts.net>
2015-07-14 21:40:50 -0400

    Add FileUtils.downloadFile

--- src/main/java/net/shadowfacts/shadowlib/util/FileUtils.java
+++ src/main/java/net/shadowfacts/shadowlib/util/FileUtils.java
@@ -1,7 +1,15 @@
 package net.shadowfacts.shadowlib.util;
 
-import java.io.File;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channel;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
 import java.util.function.Consumer;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * Various utilities related to the file system.
@@ -26,4 +34,29 @@ public class FileUtils {
 		}
 	}
 
+	/**
+	 * Downloads a file from the specified URL to the destination.
+	 * @param url
+	 * @param destination
+	 * @throws IOException Thrown if the URL is invalidm the input stream cannot be opened, the output file cannot be found, problem transferring the file
+	 */
+	public static void downloadFile(String url, String destination) throws IOException {
+		downloadFile(new URL(url), destination);
+	}
+
+	/**
+	 * Downloads a file from the specified URL to the destination.
+	 * @param url
+	 * @param destination
+	 * @throws IOException Thrown if the input stream cannot be opened, the output file cannot be found, problem transferring the file
+	 */
+	public static void downloadFile(URL url, String destination) throws IOException {
+		ReadableByteChannel in = Channels.newChannel(url.openStream());
+
+		File parent = new File(destination).getParentFile();
+		if (!parent.exists()) parent.mkdirs();
+
+		FileChannel out = new FileOutputStream(destination).getChannel();
+		out.transferFrom(in, 0, Long.MAX_VALUE);
+	}
 }
