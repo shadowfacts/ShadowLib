bdb5466657e241fe52ef317e92f4fa9f6880d22c
Shadowfacts <me@shadowfacts.net>
2015-07-31 10:39:43 -0400

    Add StringUtils

---
+++ src/main/java/net/shadowfacts/shadowlib/util/StringUtils.java
@@ -0,0 +1,29 @@
+package net.shadowfacts.shadowlib.util;
+
+import java.util.List;
+
+/**
+ * @author shadowfacts
+ */
+public class StringUtils {
+
+	public static String join(List<String> list, String joinString) {
+		return join(list.toArray(new String[0]), joinString);
+	}
+
+	public static String join(String[] list, String joinString) {
+		String ret = null;
+
+		for (int i = 0; i < list.length; i++) {
+			if (i == 0) {
+				ret = list[i];
+			} else {
+				ret += joinString;
+				ret += list[i];
+			}
+		}
+
+		return ret;
+	}
+
+}
