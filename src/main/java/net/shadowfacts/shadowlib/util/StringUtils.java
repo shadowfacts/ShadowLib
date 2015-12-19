bb33553fc25daed6acc8f2fd155b935fbaca047e
Shadowfacts <me@shadowfacts.net>
2015-12-19 09:44:07 -0500

    Add StringUtils.containsOne

--- src/main/java/net/shadowfacts/shadowlib/util/StringUtils.java
+++ src/main/java/net/shadowfacts/shadowlib/util/StringUtils.java
@@ -7,8 +7,29 @@ import java.util.List;
  */
 public class StringUtils {
 
+	/**
+	 * Counts the number of times a string occurs inside another string
+	 * @param str
+	 * @param countStr
+	 * @return
+	 */
 	public static int count(String str, String countStr) {
 		return (str.length() - str.replace(countStr, "").length()) / countStr.length();
+	}
+
+	/**
+	 * Check if the given string contains 1 of the given group of strings
+	 * @param str
+	 * @param strings
+	 * @return
+	 */
+	public static boolean containsOne(String str, Iterable<String> strings) {
+		for (String s : strings) {
+			if (str.contains(s)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
--- src/test/java/net/shadowfacts/shadowlib/util/StringUtilsTest.java
+++ src/test/java/net/shadowfacts/shadowlib/util/StringUtilsTest.java
@@ -3,6 +3,7 @@ package net.shadowfacts.shadowlib.util;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,6 +16,20 @@ public class StringUtilsTest {
 	@Test
 	public void testCount() {
 		assertEquals(StringUtils.count("hello world", "l"), 3);
+		assertEquals(StringUtils.count("hello world", "z"), 0);
+		assertEquals(StringUtils.count("hello world", "ll"), 1);
+	}
+
+	@Test
+	public void testContainsOne() {
+		List<String> strings = new ArrayList<>();
+		strings.add("a");
+		strings.add("b");
+		strings.add("c");
+		assertTrue(StringUtils.containsOne("abc", strings));
+		assertTrue(StringUtils.containsOne("abz", strings));
+		assertTrue(StringUtils.containsOne("ayz", strings));
+		assertFalse(StringUtils.containsOne("xyz", strings));
 	}
 
 }
