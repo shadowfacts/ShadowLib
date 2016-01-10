94084031f1e070cf48393662d40d7c6a11655e94
Shadowfacts <me@shadowfacts.net>
2016-01-09 20:50:16 -0500

    Generify getNextValue

--- src/main/java/net/shadowfacts/shadowlib/util/EnumUtils.java
+++ src/main/java/net/shadowfacts/shadowlib/util/EnumUtils.java
@@ -5,11 +5,11 @@ package net.shadowfacts.shadowlib.util;
  */
 public class EnumUtils {
 
-	public static Enum getNextValue(Enum current) {
+	public static <E extends Enum> E getNextValue(E current) {
 		int currentId = current.ordinal();
 		Enum[] values = current.getClass().getEnumConstants();
 		int next = currentId + 1 > values.length - 1 ? 0 : currentId + 1;
-		return values[next];
+		return (E)values[next];
 	}
 
 }
